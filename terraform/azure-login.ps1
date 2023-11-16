9bc54b90a80fc602fd96c4e448c1dec4c0fb258e
Alexey Vitvor <a.vitvor@syberry.com>
2023-11-16 21:13:05 +0300

    Add DB description

--- terraform/azure-login.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [Parameter(Mandatory = $true)] 
-    [string] $userName, 
-    [Parameter(Mandatory = $true)]
-    [SecureString] $password)
-
-az login -u $userName -p $password
-
--- terraform/stacks/web-app/outputs.tf
+++
@@ -1,8 +0,0 @@
-output app_service_id {
-  value = azurerm_app_service.web_app.id
-}
-
-output app_service_plan_id {
-  value = azurerm_app_service_plan.web_app.id
-}
-
